c673b1f9c1579220f927a8201ddc0c80223b1d80
SaboorSohaib <abdulsaboorsohaib.com>
2023-01-17 21:40:42 +0430

    fix linter

--- student.rb
+++ student.rb
@@ -2,7 +2,7 @@ require_relative 'person'
 
 class Student < Person
   attr_reader :classroom
-  
+
   def initialize(classroom, age, name = 'Unknown', parents_permission: true)
     super(age, name, parents_permission: parents_permission)
     @classroom = classroom

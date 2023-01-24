ca02e44403731a771679c89077d58c7b3f96191d
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-01-24 12:57:02 +0430

    remove commnet

--- app.rb
+++ app.rb
@@ -60,8 +60,6 @@ class App
     age = gets.chomp
     puts 'Name:'
     name = gets.chomp
-    # puts 'permission:'
-    # permission = gets.chomp
     teach = Teacher.new(specilaze, age, name)
     @people.push(teach)
   end

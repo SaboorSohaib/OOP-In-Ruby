250aab596f522e64525b02c6dcb9989693a3bb34
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-01-27 20:59:13 +0430

    Add more data

--- teacher.rb
+++ teacher.rb
@@ -3,8 +3,8 @@ require_relative 'person'
 class Teacher < Person
   attr_reader :specialization
 
-  def initialize(specialization, age, name, parents_permission: true)
-    super(age, name, parents_permission: parents_permission)
+  def initialize(specialization, age, name, parents_permission: true, id: 0)
+    super(age, name, id, parents_permission: parents_permission)
     @specialization = specialization
   end
 
